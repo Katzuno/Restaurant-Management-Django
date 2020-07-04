c4453a200f8487a573790d12a2b72d4a6cd5e1c4
Katzuno <erikhenning98@hotmail.com>
2020-07-04 18:30:42 +0300

    Started to create Nutritional Information views

--- restaurant_management_django/restaurant_management_django/nutritionals/views.py
+++ restaurant_management_django/restaurant_management_django/nutritionals/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from restaurant_management_django.restaurant_management_django.nutritionals.models import NutritionalInformation
+
 
 # Create your views here.
+
+
+class NutritionalInformationDetailView(LoginRequiredMixin, DetailView):
+    model = NutritionalInformation()
+    slug_field = "username"
+    slug_url_kwarg = "username"
+
+
+nutritional_information_detail_view = NutritionalInformationDetailView.as_view()
--- restaurant_management_django/restaurant_management_django/products/models.py
+++ restaurant_management_django/restaurant_management_django/products/models.py
@@ -4,16 +4,15 @@ from django.db.models import CharField
 from django.utils.translation import gettext_lazy as _
 
 
-class NutritionalInformation(models.Model):
+class Product(models.Model):
     class Status(models.TextChoices):
         INACTIVE = _('INACTIVE')
         ACTIVE = _('ACTIVE')
 
     name = CharField(_("Name of the product"), blank=False, max_length=255)
     description = CharField(_("Description of the product"), blank=True, max_length=1024)
-    # Find a better solution
-    #nutritional_values = CharField(_("Nutritional values of the product"), blank=True, max_length=1024)
-    nutritional_values = HStoreField()
+    # Add constraints
+    nutritional_values = HStoreField(_("Nutritional values of the product"), blank=True)
     status = models.CharField(
         max_length=8,
         choices=Status.choices,
